9d8f17ac132a83141736443c44753f92ad9eb6cf
Karan Keswani <k_keswan@encs.concordia.ca>
2020-03-23 18:45:20 -0400

    adjustments

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,7 @@ const modal = (props) => (
         <div
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'trasnaleY(-100vh)',
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
                 opacity: props.show ? '1' : '0'
             }}>
             {props.children}

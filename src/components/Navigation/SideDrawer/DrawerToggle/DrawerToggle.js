8a49e08c8ae2339df62876c0fa92c79e2fa2357c
Karan Keswani <k_keswan@encs.concordia.ca>
2020-03-23 18:45:08 -0400

    added sideDrawer toggle button

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import classes from './DrawerToggle.css'
 const drawerToggle =(props)=>(
-    <div onClick={props.clicked}>
-        MENU
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 );
 

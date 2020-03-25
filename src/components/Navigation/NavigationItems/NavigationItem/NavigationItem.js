46ea904ca153c44e0fc8cbfbb44579ab5e36df37
Karan Keswani <k_keswan@encs.concordia.ca>
2020-03-25 18:49:08 -0400

    adjustments

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './NavigationItem.css';
 const navigatioItem=(props)=>(
     <li className={classes.NavigationItem}>
-        <a href={props.link} classNam={props.active ? classes.active : null}>{props.children}</a>
+        <a href={props.link} className={props.active ? classes.active : null}>{props.children}</a>
     </li>
 );
 
